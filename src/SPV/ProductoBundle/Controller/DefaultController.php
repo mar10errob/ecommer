a30ad8c193bbabfbb322fa69b68f092e4382b79a
Mario Alberto <malberto1927@gmail.com>
2014-06-25 21:09:23 -0500

    Creacion de la Portada

--- src/SPV/ProductoBundle/Controller/DefaultController.php
+++ src/SPV/ProductoBundle/Controller/DefaultController.php
@@ -6,8 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('ProductoBundle:Default:index.html.twig', array('name' => $name));
+    public function portadaAction(){
+        return $this->render('ProductoBundle:Default:poartada.html.twig');
     }
 }
